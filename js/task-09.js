468b6b165736f1c04a8c875fb8f5ef4e6fb93580
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-11-10 22:23:04 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,24 @@
 function getRandomHexColor() {
+
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
 };
 
+const changeColorBtn = document.querySelector('button[type="button"]');
 
+const spanEl = document.querySelector('span');
+
+const bodyEl = document.querySelector('body');
+
+const onChangeColorBtnClick = function () {
+  
+  let myColor = getRandomHexColor();
+
+  spanEl.textContent = myColor;
+
+  bodyEl.style.backgroundColor = myColor;
+
+}
+
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
